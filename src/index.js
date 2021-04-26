9aefd8de0810335276611a5f4f9ee9167af200db
Palash Gupta <palashguptakatni@gmail.com>
2021-04-26 16:22:48 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 // Loading routes
 app.use(userRouter)
